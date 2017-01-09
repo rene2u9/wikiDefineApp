af86683f45e9950671afe0b0fb79e9167948bb70
rene2u9 <rene2u9@gmail.com>
2017-01-09 15:21:54 +0100

    added multilang support

--- app/src/main/java/tm16wiki/wikidefine/MainActivity.java
+++ app/src/main/java/tm16wiki/wikidefine/MainActivity.java
@@ -1,31 +1,22 @@
 package tm16wiki.wikidefine;
 
-import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.StrictMode;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import javax.net.ssl.SNIServerName;
+import android.view.View;
 
 import tm16wiki.wikidefine.databinding.ActivityMainBinding;
-import tm16wiki.wikidefine.wikiAPI.wikiTextParser;
 
 public class MainActivity extends AppCompatActivity {
-    
+
     private static final String TAG = "MainActivity";
 
     /**
@@ -34,37 +25,6 @@ public class MainActivity extends AppCompatActivity {
     private WikiProxy wikiProxy;
     private ActivityMainBinding activityMainBinding;
 
-    /**
-     * @author: Harry Kituyi
-     * A Handler for our Proxy from the UI
-     */
-    public  class MainActivityHandler {
-        private final Context mAppContext;
-
-        /**
-         * Constructor for the databinding handler
-         *
-         * @param context pass an activity for some methods that may need it
-         */
-        MainActivityHandler(Context context) {
-            mAppContext = context;
-        }
-
-        public void query(View view){
-             Editable editable = activityMainBinding.contentMainLayout.editText.getText();
-            if(TextUtils.isEmpty(editable)){
-                Snackbar.make(view,R.string.insert_query,Snackbar.LENGTH_SHORT).show();
-            }else {
-                    activityMainBinding.getWikiProxy().new ContentLoader(editable).execute();
-            }
-        }
-
-
-
-
-    }
-    
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         All the magic happens below
          */
-        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         wikiProxy = WikiProxy.getWikiProxy();
         wikiProxy.setmRetrieverListener(new WikiProxy.RetrieverListener() {
             @Override
@@ -99,31 +59,82 @@ public class MainActivity extends AppCompatActivity {
         activityMainBinding.contentMainLayout.outputTextView.setMovementMethod(new ScrollingMovementMethod());
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-
-            return true;
+        switch (id) {
+            case R.id.setLang_de:
+                wikiProxy.setLang("de");
+                break;
+            case R.id.setLang_en:
+                wikiProxy.setLang("en");
+                break;
+            case R.id.setLang_fr:
+                wikiProxy.setLang("fr");
+                break;
+            case R.id.setLang_es:
+                wikiProxy.setLang("es");
+                break;
+            case R.id.setLang_ru:
+                wikiProxy.setLang("ru");
+                break;
+            case R.id.setLang_it:
+                wikiProxy.setLang("it");
+                break;
+            case R.id.setLang_pl:
+                wikiProxy.setLang("pl");
+                break;
+            case R.id.setLang_pt:
+                wikiProxy.setLang("pt");
+                break;
+            case R.id.setLang_zh:
+                wikiProxy.setLang("zh");
+                break;
+            case R.id.setLang_ja:
+                wikiProxy.setLang("ja");
+                break;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    /**
+     * @author: Harry Kituyi
+     * A Handler for our Proxy from the UI
+     */
+    public class MainActivityHandler {
+        private final Context mAppContext;
+
+        /**
+         * Constructor for the databinding handler
+         *
+         * @param context pass an activity for some methods that may need it
+         */
+        MainActivityHandler(Context context) {
+            mAppContext = context;
+        }
+
+        public void query(View view) {
+            Editable editable = activityMainBinding.contentMainLayout.editText.getText();
+            if (TextUtils.isEmpty(editable)) {
+                Snackbar.make(view, R.string.insert_query, Snackbar.LENGTH_SHORT).show();
+            } else {
+                activityMainBinding.getWikiProxy().new ContentLoader(editable).execute();
+            }
+        }
+
     }
 }
--- app/src/main/java/tm16wiki/wikidefine/WikiProxy.java
+++ app/src/main/java/tm16wiki/wikidefine/WikiProxy.java
@@ -12,74 +12,47 @@ import tm16wiki.wikidefine.wikiAPI.wikiTextParser;
 
 /**
  * Created by harryk on 1/9/17.
- *@author Harry K.
  *
- *
- * A Proxy Class to manage The Backend, In Singleton design (for better Managing)
- *
- * I'm not that good with Threading and such, so if i break it here please inform me but i just Reimplemented the Algorithm
- * This way we can have only one Object to manage Loading of queries,
- *          We can encapsulate the (Somewhat Cryptic) Algorithms
- *          Separate UIspecific Code from Application LOgic
- *
- *          I love Design Patterns ;)
+ * @author Harry K.
+ *         <p>
+ *         <p>
+ *         A Proxy Class to manage The Backend, In Singleton design (for better Managing)
+ *         <p>
+ *         I'm not that good with Threading and such, so if i break it here please inform me but i just Reimplemented the Algorithm
+ *         This way we can have only one Object to manage Loading of queries,
+ *         We can encapsulate the (Somewhat Cryptic) Algorithms
+ *         Separate UIspecific Code from Application LOgic
+ *         <p>
+ *         I love Design Patterns ;)
  */
 
-public class WikiProxy extends BaseObservable{
+public class WikiProxy extends BaseObservable {
     /**
      * URL To retrieve the content
      */
-    private final static String URL = "https://en.wikipedia.org/wiki/Special:Export/";
+    private final static String URL = "wikipedia.org/wiki/Special:Export/";
     /**
      * Text field
      */
     private final static String TEXT = "text";
-
     /**
      * Required length for proper definition i suppose
      */
     private final static int REQ_LENGTH = 30;
-
     private static final String TAG = "DebugTag";
-
     /**
-     * Exception Thrown when we dont have ant results
+     *
      */
-    private class NotFoundException extends Exception{
-        NotFoundException(String message) {
-            super(message);
-        }
-    }
-
-    /**
-     * Exception Thrown when we dont Have any Connection (https.loadurl(..) returns false)
-     */
-    private class HttpsConnectionException extends Exception{
-        HttpsConnectionException(String message) {
-            super(message);
-        }
-    }
-
-    /**
-     * A Listener Interface For Checking for results when we have a connection
-     * Might be handy with Progress Bars ,etc
-     */
-    interface RetrieverListener {
-        void onLoad();
-        void onFinish();
-        void onReceiveException(String exceptionMessage);
-    }
-
+    private static String LANG = "en";
     /**
      * References to Composited objects
      */
     private static WikiProxy sWikiProxy;
     private tm16wiki.wikidefine.helperClasses.xml XmlHelper;
-    private tm16wiki.wikidefine.helperClasses.https HttpsHelper ;
+    private tm16wiki.wikidefine.helperClasses.https HttpsHelper;
     private wikiTextParser wikiTextParser;
     private RetrieverListener mRetrieverListener;
     private String result;
-
     /**
      * private Constructor, because Singleton
      */
@@ -91,27 +64,41 @@ public class WikiProxy extends BaseObservable{
     }
 
     /**
+     * How we Obtain our Singleton Proxy Object
+     *
+     * @return a reference to our Static object if present
+     */
+    static WikiProxy getWikiProxy() {
+        if (sWikiProxy == null) {
+            sWikiProxy = new WikiProxy();
+        }
+        return sWikiProxy;
+    }
+
+    /**
      * A getter method returning the current result
      * bindable annotation makes databinding monitor changes, No more Findviewbyid :)
+     *
      * @return current result of last succesful query
      */
     @Bindable
-    public String getResult(){
+    public String getResult() {
         return result;
     }
 
     /**
      * set Result from succesful query
      *
-     * @param result  query result
+     * @param result query result
      */
-    public void setResult(String result){
+    public void setResult(String result) {
         this.result = result;
         notifyPropertyChanged(tm16wiki.wikidefine.BR.result);
     }
 
     /**
      * Implement to listen for results
+     *
      * @param mRetrieverListener Callback interface
      */
     void setmRetrieverListener(RetrieverListener mRetrieverListener) {
@@ -119,105 +106,73 @@ public class WikiProxy extends BaseObservable{
     }
 
     /**
-     * How we Obtain our Singleton Proxy Object
-     *
-     * @return  a reference to our Static object if present
-     */
-    static WikiProxy getWikiProxy(){
-        if(sWikiProxy == null){
-            sWikiProxy = new WikiProxy();
-        }
-        return sWikiProxy;
-    }
-
-    /**
      * Call to get The content for parsing
+     *
      * @param editable from getText method of editext
      * @return The definitions
-     * @throws NotFoundException When the result is not there
+     * @throws NotFoundException        When the result is not there
      * @throws HttpsConnectionException When theres no connection
      */
     private String retrieveContent(Editable editable) throws NotFoundException, HttpsConnectionException {
-        if(HttpsHelper.loadURL(URLBuilder(editable))){
-            String definition = wikiTextParser.getDefinition(XmlHelper.getTagValue(HttpsHelper.getContent(),TEXT));
-            if(definition.length() < REQ_LENGTH){
-                throw  new NotFoundException("Not Found");
-            }else {
+        if (HttpsHelper.loadURL(URLBuilder(editable))) {
+            String definition = wikiTextParser.getDefinition(XmlHelper.getTagValue(HttpsHelper.getContent(), TEXT));
+            if (definition.length() < REQ_LENGTH) {
+                throw new NotFoundException("Not Found");
+            } else {
                 return definition;
             }
-        }else {
+        } else {
             throw new HttpsConnectionException("No Connection");
         }
     }
 
     /**
-     * AsyncTask that loads the information in a simple background thread without blocking UI,
-     * We Can Implement a HandlerThread if you want more Flexibility, But for now , this Works
-     */
-    class ContentLoader extends AsyncTask<Void,Void,String>{
-        private final Editable ed;
-        private boolean isRethrowException;
-        private String exceptionMessage = null;
-
-        /**
-         * Pass your query here
-         * @param param content to query
-         */
-        ContentLoader(Editable param) {
-            ed = param;
-            isRethrowException = false;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            if(mRetrieverListener != null){
-                mRetrieverListener.onLoad();
-            }
-        }
-
-        @Override
-        protected String doInBackground(Void... params) {
-            try {
-                return retrieveContent(ed);
-            } catch (NotFoundException | HttpsConnectionException e) {
-                exceptionMessage = e.getMessage();
-                isRethrowException = true;
-            }
-            return " ";
-        }
-
-
-        @Override
-        protected void onPostExecute(String definition) {
-            super.onPostExecute(definition);
-            if(mRetrieverListener != null){
-                mRetrieverListener.onFinish();
-            }
-            if(isRethrowException) {
-                if (mRetrieverListener != null) {
-                    mRetrieverListener.onReceiveException(exceptionMessage);
-                }
-            }
-            setResult(SanitizeDefinition(definition));
-        }
-
-
-    }
-
-
-
-    /**
      * Last section of that Algorithm separated for clarity
+     *
      * @param def String to sanitize
      * @return Sanitized string
      */
-    private String SanitizeDefinition(String def){
-        while (def.indexOf(" ")==0){
-            def =def.substring(1, def.length());
+    private String SanitizeDefinition(String def) {
+        while (def.indexOf(" ") == 0) {
+            def = def.substring(1, def.length());
         }
         Log.d(TAG, "SanitizeDefinition: " + def);
         return def.replace("*", "\n*");
+    }
+
+    /**
+     * Returns a Concatenated full URL component, Can add extra sanitizer of input
+     *
+     * @param query extra part
+     * @return full URL
+     */
+    private String URLBuilder(Editable query) {
+        return "https://" + LANG + "." + URL + query;
+    }
+
+    protected void setLang(String lang) {
+        LANG = lang;
+    }
+
+    /**
+     * A Listener Interface For Checking for results when we have a connection
+     * Might be handy with Progress Bars ,etc
+     */
+    interface RetrieverListener {
+        void onLoad();
+
+        void onFinish();
+
+        void onReceiveException(String exceptionMessage);
+    }
+
+    /**
+     * Exception Thrown when we dont have ant results
+     */
+    private class NotFoundException extends Exception {
+        NotFoundException(String message) {
+            super(message);
+        }
     }
 
     /*
@@ -242,14 +197,68 @@ public class WikiProxy extends BaseObservable{
     }*/
 
     /**
-     * Returns a Concatenated full URL component, Can add extra sanitizer of input
-     * @param query extra part
-     * @return full URL
+     * Exception Thrown when we dont Have any Connection (https.loadurl(..) returns false)
      */
-    private String URLBuilder(Editable query){
-        return URL + query;
+    private class HttpsConnectionException extends Exception {
+        HttpsConnectionException(String message) {
+            super(message);
+        }
     }
 
+    /**
+     * AsyncTask that loads the information in a simple background thread without blocking UI,
+     * We Can Implement a HandlerThread if you want more Flexibility, But for now , this Works
+     */
+    class ContentLoader extends AsyncTask<Void, Void, String> {
+        private final Editable ed;
+        private boolean isRethrowException;
+        private String exceptionMessage = null;
 
+        /**
+         * Pass your query here
+         *
+         * @param param content to query
+         */
+        ContentLoader(Editable param) {
+            ed = param;
+            isRethrowException = false;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            if (mRetrieverListener != null) {
+                mRetrieverListener.onLoad();
+            }
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                return retrieveContent(ed);
+            } catch (NotFoundException | HttpsConnectionException e) {
+                exceptionMessage = e.getMessage();
+                isRethrowException = true;
+            }
+            return " ";
+        }
+
+
+        @Override
+        protected void onPostExecute(String definition) {
+            super.onPostExecute(definition);
+            if (mRetrieverListener != null) {
+                mRetrieverListener.onFinish();
+            }
+            if (isRethrowException) {
+                if (mRetrieverListener != null) {
+                    mRetrieverListener.onReceiveException(exceptionMessage);
+                }
+            }
+            setResult(SanitizeDefinition(definition));
+        }
+
+
+    }
 
 }
