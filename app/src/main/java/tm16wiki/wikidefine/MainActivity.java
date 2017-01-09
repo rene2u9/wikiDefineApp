918f9b0fc850dcecd8edc0429b97b899779ea1b1
Harry Kituyi <kituyiharry@gmail.com>
2017-01-09 13:50:52 +0300

    Wrapped Backend Implementation into a proxy object, Implemented A simple Background thread with AsyncTask Applied DataBinding to MainActivity

--- app/src/main/java/tm16wiki/wikidefine/MainActivity.java
+++ app/src/main/java/tm16wiki/wikidefine/MainActivity.java
@@ -1,72 +1,104 @@
 package tm16wiki.wikidefine;
 
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import javax.net.ssl.SNIServerName;
+
+import tm16wiki.wikidefine.databinding.ActivityMainBinding;
 import tm16wiki.wikidefine.wikiAPI.wikiTextParser;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private static final String TAG = "MainActivity";
 
-    private EditText searchtext;
-    private TextView textview;
+    /**
+     * i'll replace these, A Databinding component will handle them, Makes Code decoupled and less :)
+     */
+    private WikiProxy wikiProxy;
+    private ActivityMainBinding activityMainBinding;
+
+    /**
+     * @author: Harry Kituyi
+     * A Handler for our Proxy from the UI
+     */
+    public  class MainActivityHandler {
+        private final Context mAppContext;
+
+        /**
+         * Constructor for the databinding handler
+         *
+         * @param context pass an activity for some methods that may need it
+         */
+        MainActivityHandler(Context context) {
+            mAppContext = context;
+        }
+
+        public void query(View view){
+             Editable editable = activityMainBinding.contentMainLayout.editText.getText();
+            if(TextUtils.isEmpty(editable)){
+                Snackbar.make(view,R.string.insert_query,Snackbar.LENGTH_SHORT).show();
+            }else {
+                    activityMainBinding.getWikiProxy().new ContentLoader(editable).execute();
+            }
+        }
 
 
-    tm16wiki.wikidefine.helperClasses.xml xml = new tm16wiki.wikidefine.helperClasses.xml();
-    tm16wiki.wikidefine.helperClasses.https https = new tm16wiki.wikidefine.helperClasses.https();
-    wikiTextParser text = new wikiTextParser();
+
+
+    }
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
         setContentView(R.layout.activity_main);
-
-        searchtext = (EditText) findViewById(R.id.editText);
-        textview = (TextView) findViewById(R.id.outputTextView);
-        textview.setMovementMethod(new ScrollingMovementMethod());
-
-        View.OnClickListener action = new View.OnClickListener() {
+        All the magic happens below
+         */
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        wikiProxy = WikiProxy.getWikiProxy();
+        wikiProxy.setmRetrieverListener(new WikiProxy.RetrieverListener() {
             @Override
-            public void onClick(View view) {
-
-                Snackbar.make(view, "lade...", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-
-                if(https.loadURL("https://en.wikipedia.org/wiki/Special:Export/"+ searchtext.getText())){
-                    String content = xml.getTagValue(https.getContent(), "text");
-                    String definition = text.getDefinition(content);
-                    if(definition.length() < 30){
-                        Snackbar.make(view, "nichts gefunden ¯\\_(ツ)_/¯", Snackbar.LENGTH_LONG)
-                                .setAction("Action", null).show();
-
-                    }else {
-                        while (definition.indexOf(" ")==0){
-                            definition =definition.substring(1, definition.length());
-                        }
-                        textview.setText(definition.replace("*", "\n*"));
-                    }
-                }else{
-                    Snackbar.make(view, "nichts gefunden ¯\\_(ツ)_/¯", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-
-                }
-
-
+            public void onLoad() {
+                Log.d(TAG, "onLoad: Loading");
+                activityMainBinding.contentMainLayout.contentProgressBar.setVisibility(View.VISIBLE);
             }
-        };
 
+            @Override
+            public void onFinish() {
+                Log.d(TAG, "onFinish: Done loading");
+                activityMainBinding.contentMainLayout.contentProgressBar.setVisibility(View.GONE);
+            }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(action);
+            @Override
+            public void onReceiveException(String exceptionMessage) {
+                Snackbar.make(activityMainBinding.getRoot(),
+                        getResources().getString(R.string.not_found),
+                        Snackbar.LENGTH_SHORT).show();
+            }
+        });
+        activityMainBinding.setWikiProxy(wikiProxy);
+        activityMainBinding.setMainActivityHandler(new MainActivityHandler(this));
+        activityMainBinding.contentMainLayout.outputTextView.setMovementMethod(new ScrollingMovementMethod());
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -87,7 +119,11 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
